ab9019bf2a0fcac880e2e0ed537770f8127a3bfc
Mikec <mikecitt@gmail.com>
2020-04-17 21:50:55 +0200

    Inicijalna verzija sa osnovnom funkcionalnosti projekta.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $.getJSON("data/city.list.json") {
+
+  }
+});
+
+function posalji() {
+  var naziv = $("#naziv").val();
+  $("#data").empty();
+  console.log(naziv);
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + naziv + "&APPID=39314e0ae6d3990ee9fa61eca7f546a9",
+    type: "GET",
+    dataType: "json",
+    complete: function(data) {
+      console.log(data.responseJSON);
+      $("#div").append("")
+      $.each(data.responseJSON.list, function(index, value) {
+        var row =
+        `
+        <tr>
+          <td>${value.dt_txt}</td>
+          <td>${(value.main.temp-273.15).toFixed(2)}</td>
+          <td>${(value.main.temp_max-273.15).toFixed(2)}</td>
+          <td>${(value.main.temp_min-273.15).toFixed(2)}</td>
+          <td>${value.main.pressure}</td>
+          <td>${value.main.humidity}</td>
+          <td>100%</td>
+        </tr>
+        `;
+        $("#data").append(row)
+      });
+    }
+  });
+}
